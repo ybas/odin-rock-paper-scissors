068b9b3f0b2056f0646cdd57f461a6eb57deae69
ybas <ybas2627@gmail.com>
2026-03-26 17:05:08 +0700

    displaying results with DOM method

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const playButton = document.getElementById("play-button");
+const $$ = (selector) => document.getElementById(selector);
+const displayYourChoice = $$("your-choice");
+const displayComputerChoice = $$("computer-choice");
+const displayResult = $$("result");
+const displayYourScore = $$("your-score");
+const displayTieScore = $$("tie-score");
+const displayComputerScore = $$("computer-score");
+const hiddenSection = document.querySelectorAll(".hidden-section");
 
 const optionChoice = ["Rock", "Paper", "Scissors"];
 let humanScore = 0;
 let computerScore = 0;
 let tieScore = 0;
 
-const getHumanChoice = function(i) {
-   let getHumanInput = prompt("To play, please type between rock, paper, or scissors.");
-   console.log(`Round (${i})`);
-   if (optionChoice.includes(capitalize(getHumanInput))) {
-      console.log(`Your Choice: ${capitalize(getHumanInput)}`);
-      return capitalize(getHumanInput);
+const getyourChoice = function(paramsChoice) {
+   if (optionChoice.includes(capitalize(paramsChoice))) {
+      return capitalize(paramsChoice);
    } else {
-      console.log(`Because your choice is invalid, it is selected automatically: ${getComputerChoice()}`);
       return getComputerChoice();
    }
 }
@@ -22,32 +25,58 @@ const getComputerChoice = function() {
    return optionChoice[randomIndex];
 }
 
-const playRound = function(humanChoice, computerChoice) {
-   console.log(`Computer Choice: ${computerChoice}`);
-   if (humanChoice === computerChoice) {
-      console.log("Results: Tie");
+const playRound = function(yourChoice, computerChoice) {
+   if (yourChoice === computerChoice) {
+      displayResult.textContent = "Tie"
       tieScore++;
    } else if (
-      (humanChoice === "Rock" && computerChoice === "Scissors") ||
-      (humanChoice === "Paper" && computerChoice === "Rock") ||
-      (humanChoice === "Scissors" && computerChoice === "Paper") 
+      (yourChoice === "Rock" && computerChoice === "Scissors") ||
+      (yourChoice === "Paper" && computerChoice === "Rock") ||
+      (yourChoice === "Scissors" && computerChoice === "Paper") 
    ) {
-      console.log("Results: You Win");
+      displayResult.textContent = "You Win"
       humanScore++;
    } else {
-      console.log("Results: You Lose");
+      displayResult.textContent = "You Lose"
       computerScore++;
    }
-   console.log(`Your Score: ${humanScore} | Computer Score: ${computerScore} | Tie: ${tieScore}`);
+   updateScore(yourChoice, computerChoice);
 }
 
-playButton.addEventListener("click", () => {
-   humanScore = 0;
-   computerScore = 0;
-   tieScore = 0;
-   console.clear();
-   for (let i = 1; i <= 5; i++) {
-      playRound(getHumanChoice(i), getComputerChoice());
+const updateScore = function(yourChoice, computerChoice) {
+   displayYourChoice.textContent = yourChoice;
+   displayComputerChoice.textContent = computerChoice;
+   displayYourScore.textContent = humanScore;
+   displayTieScore.textContent = tieScore;
+   displayComputerScore.textContent = computerScore;
+   hiddenSection.forEach(el => {
+      el.style.display = 'block';
+   });
+   if (humanScore === 5) {
+      alert("You are the winner");
+      disableBtn();
+   } else if (computerScore === 5) {
+      alert("The computer is the winner");
+      disableBtn();
+   }
+}
+
+const disableBtn = function() {
+   document.querySelectorAll(".choice-btn").forEach(el => {
+      el.disabled = true;
+   });
+}
+
+document.querySelector('.select-shape-section').addEventListener('click', (event) => {
+   const {target} = event;
+
+   if(!target.matches('button')){
+      return;
+   }
+
+   if(target.classList.contains('choice-btn')){
+      playRound(getyourChoice(target.value), getComputerChoice());
+      return;
    }
 });
 
